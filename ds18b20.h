9909176836ad7a73766c4f20d09c663347a938cd
David Lehrian <david@lehrian.com>
2021-08-18 18:39:14 -0700

    Support multiple devices
    Updated to support multiple ds18b20 devices and provide for configuration of those devices.  Can get the temperature in Celcius and Farenheit.

--- ds18b20.h
+++ ds18b20.h
@@ -15,12 +15,42 @@
 #ifndef DS18B20_H_  
 #define DS18B20_H_
 
-void ds18b20_send(char bit);
-unsigned char ds18b20_read(void);
-void ds18b20_send_byte(char data);
-unsigned char ds18b20_read_byte(void);
-unsigned char ds18b20_RST_PULSE(void);
-float ds18b20_get_temp(void);
+#define noInterrupts() portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;taskENTER_CRITICAL(&mux)
+#define interrupts() taskEXIT_CRITICAL(&mux)
+
+#define DEVICE_DISCONNECTED_C -127
+#define DEVICE_DISCONNECTED_F -196.6
+#define DEVICE_DISCONNECTED_RAW -7040
+#define constrain(amt,low,high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
+#define pgm_read_byte(addr)   (*(const unsigned char *)(addr))
+
+typedef uint8_t DeviceAddress[8];
+typedef uint8_t ScratchPad[9];
+
 void ds18b20_init(int GPIO);
+
+void ds18b20_write(char bit);
+unsigned char ds18b20_read(void);
+void ds18b20_write_byte(char data);
+unsigned char ds18b20_read_byte(void);
+unsigned char ds18b20_reset(void);
+
+bool ds18b20_setResolution(const DeviceAddress tempSensorAddresses[], int numAddresses, uint8_t newResolution);
+bool ds18b20_isConnected(const DeviceAddress *deviceAddress, uint8_t *scratchPad);
+void ds18b20_writeScratchPad(const DeviceAddress *deviceAddress, const uint8_t *scratchPad);
+bool ds18b20_readScratchPad(const DeviceAddress *deviceAddress, uint8_t *scratchPad);
+void ds18b20_select(const DeviceAddress *address);
+uint8_t ds18b20_crc8(const uint8_t *addr, uint8_t len);
+bool ds18b20_isAllZeros(const uint8_t * const scratchPad);
+bool isConversionComplete();
+uint16_t millisToWaitForConversion();
+
+void ds18b20_requestTemperatures();
+float ds18b20_getTempF(const DeviceAddress *deviceAddress);
+float ds18b20_getTempC(const DeviceAddress *deviceAddress);
+int16_t calculateTemperature(const DeviceAddress *deviceAddress, uint8_t* scratchPad);
+
+void reset_search();
+bool search(uint8_t *newAddr, bool search_mode);
 
 #endif
